813312781783b5ece7fd860457e19e275c1e4954
efahmed <engineerfa.24816@gmail.com>
2019-01-11 00:36:54 +0000

    get-average is done

--- II.pass-tests/08-get-average/get-average.js
+++ II.pass-tests/08-get-average/get-average.js
@@ -1,3 +1,16 @@
 // the input is an array of numbers and strings
 // return the average of all the numbers
 // be sure to exclude the strings
+
+function average(numbers) {
+  var filtered = numbers.filter(function(x) {
+    return typeof x !== "string";
+  });
+  var sum = 0;
+  for (var i = 0; i < filtered.length; i++) {
+    sum += filtered[i];
+  }
+  var avg = sum / filtered.length;
+  return avg;
+}
+module.exports = average;
